53a003dd6053a2b45cfbd5bcbaad1c8cc2e1d738
Erclismedina <121984453+Erclismedina@users.noreply.github.com>
2023-02-21 22:05:02 -0300

    Add files via upload

---
+++ Cachureando-Sprint-M-3 - copia/README.md
@@ -0,0 +1,9 @@
+# Cachureando
+Enlace al repositorio [acá](https://github.com/EgoExMachina/Cachureando)
+## Integrantes
+* Gabriela Tonko @gabitonko
+* Erclis Medina @Erclismedina
+* Pablo López @EgoExMachina
+### Nuevo anexo
+* Erclis Medina @Erclismedina
+* Gabriela Tonko @gabitonko
---
+++ Cachureando-Sprint-M-3 - copia/js/productos.js
@@ -0,0 +1,82 @@
+  const baseDeDatos = [
+      {
+          id: 1,
+          nombre: 'Reciclaje de Papel en Oficinas',
+          descripcion: 'Ayuda con el papel',
+          precio: 25000,
+          imagen: 'images/producto-1.jpg',
+      },
+      {
+          id: 2,
+          nombre: 'Reciclaje de Electrónicos en Oficinas',
+          descripcion: 'desechar correctamente los electrónicos',
+          precio: 25000,
+          imagen: 'images/producto-2.jpg',
+      },
+      {
+          id: 3,
+          nombre: 'Reciclaje de Orgánicos en Oficinas',
+          descripcion: 'Fabricación de compost',
+          precio: 25000,
+          imagen: 'images/producto-3.jpg',
+      },
+      {
+          id: 4,
+          nombre: 'Reciclaje de Plásticos en Oficinas',
+          descripcion: 'División especifica de cada plástico',
+          precio: 25000,
+          imagen: 'images/producto-4.jpg',
+      },
+      {
+          id: 5,
+          nombre: 'Reciclaje de Vidrios en Oficinas',
+          descripcion: 'Manejo adecuado para la separación de vidrios',
+          precio: 25000,
+          imagen: 'images/producto-5.jpg',
+
+      },
+      {
+          id: 6,
+          nombre: 'Reciclaje de Metales en Oficinas',
+          descripcion: 'Manejo adecuado en la limpieza de metales',
+          precio: 25000,
+          imagen: 'images/producto-6.jpg',
+
+        },
+        {
+          id: 7,
+          nombre: 'Diseño de Área de Trabajo',
+          descripcion: 'Personalizamos tu área de trabajo',
+          precio: 25000,
+          imagen: 'images/producto-7.jpg',
+
+        },
+        {
+          id: 8,
+          nombre: 'Diseño Área de Trabajo verde',
+          descripcion: 'Colocamos plantas a tu oficina',
+          precio: 25000,
+          imagen: 'images/producto-8.jpg',
+
+        },
+        {
+            id: 9,
+            nombre: 'Diseño Área de Trabajo apto Para Mascotas',
+            descripcion: 'Un ambiente cómodo para los peludos',
+            precio: 25000,
+            imagen: 'images/producto-9.jpg',
+
+        },
+        {
+            id: 10,
+            nombre: 'Diseño Área de Descanso',
+            descripcion: 'Personalizamos según las necesidades y requisitos de tu equipo',
+            precio: 25000,
+            imagen: 'images/producto-10.jpg',
+
+        },
+
+  ];
+
+    
+
